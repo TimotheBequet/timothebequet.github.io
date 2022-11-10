3e1b40ddaf9b431380bf790f73d034272be573d3
TimotheBequet <tim.bequet@gmail.com>
2022-11-10 09:55:43 +0100

    modif animation des slides, pour passer d'une slide à l'autre dès le premier tour de molette de souris

--- js/slide.js
+++ js/slide.js
@@ -12,49 +12,34 @@ const slide = {
 
     zoomDezoomElement: function(element, zoom) {
         let opacityElement = parseFloat(slide.getOpacity(element));
-        const scaleX = element.getBoundingClientRect().width / element.offsetWidth;
         if (   (element.classList.contains('last') && zoom < 0 && opacityElement >= 1)
             || (element.classList.contains('first') && ((zoom > 0) || (zoom < 0 && opacityElement < 1)))
             || (!element.classList.contains('last') && !element.classList.contains('first'))                            
-           ) {    
-            let newScale = 0;                    
+           ) {
             if (zoom > 0) {
-                newScale = scaleX + 0.2;
-                opacityElement -= 0.2;
-            } else {
-                newScale = scaleX - 0.4;
-                opacityElement += 0.4;
-            }
-
-            if (slide.arrond(newScale) <= 1) {
-                newScale = 1;
-                element.style.opacity = 1;
-                element.style.transform = 'scale(1)';                  
-            } else {
-                if (opacityElement > 1)
-                    opacityElement = 1;
-                else if (opacityElement < 0)
-                    opacityElement = 0;  
-                    
-                element.style.opacity = opacityElement;
-                element.style.transform = 'scale(' + newScale + ')'; 
-            }
-            
-            if (opacityElement <= 0) {
-                // si l'élément courant a une opacité de 0 ou moins, 
-                //faut le faire disparaitre pour se positionner sur l'élément suivant
+                element.classList.remove('current-slide');
+                element.classList.add('zoomed');
                 let nextElement = element.nextElementSibling;  
-                if (nextElement !== null)              
-                    element.classList.add('inactive');               
-            } else if (newScale === 1 && zoom < 0) {
-                // le scale de l'élément courant est à 1, et on dézoome
-                // il faut rendre visible l'élement précédent s'il existe
+                if (nextElement !== null) {   
+                    setTimeout(() => {           
+                        element.classList.add('inactive');
+                    }, 2000);
+                    nextElement.classList.remove('unzoomed');
+                    nextElement.classList.add('current-slide');
+                }                 
+            } else {
+                element.classList.remove('current-slide');
+                element.classList.add('unzoomed');
                 let previousElement = element.previousElementSibling;
-                if (previousElement !== null) {
-                    previousElement.classList.remove('inactive');
+                console.log(previousElement);
+                if (previousElement !== null) {                    
+                    previousElement.classList.remove('inactive'); 
+                    setTimeout(() => {           
+                        previousElement.classList.remove('zoomed');  
+                        previousElement.classList.add('current-slide');                      
+                    }, 1);                    
                 }
-            }
-            
+            }            
         }        
     },
     
